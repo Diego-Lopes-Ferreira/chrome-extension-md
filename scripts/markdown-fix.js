d307aa269e62b4dc3d86bb5aeaea97cace81f43f
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-11-27 18:20:35 -0300

    fix: different types of inputs

--- scripts/markdown-fix.js
+++ scripts/markdown-fix.js
@@ -8,10 +8,20 @@ function fixReplaceEveryInputTypeChecked(content) {
   let regexForEmptyInput = '<li><input disabled="" type="checkbox">';
   let substituteForEmptyCheckbox = '<li class="haveinput"><svg viewBox="0 0 24 24" class="checkbox-icon not-checked"><circle cx="12" cy="12" r="10"></circle></svg>';
   content = content.replaceAll(regexForEmptyInput, substituteForEmptyCheckbox);
+
+  let otherRegexForEmptyInput = '<li><p><input disabled="" type="checkbox">';
+  let substituteForOtherEmptyInput = '<li class="haveinput"><p><svg viewBox="0 0 24 24" class="checkbox-icon not-checked"><circle cx="12" cy="12" r="10"></circle></svg>';
+  content = content.replaceAll(otherRegexForEmptyInput, substituteForOtherEmptyInput);
+  
   // * Replace marked checkbox
   let regexForCheckedInput = '<li><input checked="" disabled="" type="checkbox">';
   let substituteForCheckedCheckbox = '<li class="haveinput"><svg viewBox="0 0 24 24" class="checkbox-icon checked"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline class="checkmark" points="22 4 12 14.01 9 11.01"></polyline></svg>';
   content = content.replaceAll(regexForCheckedInput, substituteForCheckedCheckbox);
+
+  let otherRegexForCheckedInput = '<li><p><input checked="" disabled="" type="checkbox">';
+  let otherSubstituteForCheckedCheckbox = '<li class="haveinput"><p><svg viewBox="0 0 24 24" class="checkbox-icon checked"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline class="checkmark" points="22 4 12 14.01 9 11.01"></polyline></svg>';
+  content = content.replaceAll(otherRegexForCheckedInput, otherSubstituteForCheckedCheckbox);
+  
   console.log('replacing');
   console.log(content);
   return content;
